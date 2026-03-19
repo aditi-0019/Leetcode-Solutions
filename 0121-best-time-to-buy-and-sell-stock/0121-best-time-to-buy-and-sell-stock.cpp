e42c59cf67dd49f2e9904f1d857b598d56a76bb8
Aditi Singh <singhaditi.as75@gmail.com>
2026-03-19 22:33:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int maxprofit=0,minprice=prices[0];
+        if(prices.empty()) return 0;
         for (int i=1;i<prices.size();i++){
             if(minprice <prices[i]){
                 maxprofit=max(maxprofit,prices[i]-minprice);
