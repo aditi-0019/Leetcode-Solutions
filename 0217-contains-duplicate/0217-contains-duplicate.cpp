7ce3d1f37b0500ea11665b4acb13ed35c2c5a09f
Aditi Singh <singhaditi.as75@gmail.com>
2026-03-21 23:24:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,17 +1,15 @@
-#include <unordered_set>
-#include <vector>
 using namespace std;
 
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> s;
-        
-        for(int num : nums) {
-            if(s.count(num)) return true;
-            s.insert(num);
+    bool containsDuplicate(vector<int>& nums) 
+    {
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if(nums[i] == nums[i - 1])
+                return true;
         }
-        
         return false;
     }
 };
