64c83f12b5d6e84ff8811d4b13e21053701adcb2
Aditi Singh <singhaditi.as75@gmail.com>
2026-03-21 23:21:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,15 +1,6 @@
-#include<set>
-
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        set<int> s;
-        for(int i=0;i<nums.size();i++){
-            s.insert(nums[i]);
-        }
-       if(s.size()==nums.size()) {
-       return false ;}
-       else return true;
-
+        return nums.size() > set<int>(nums.begin(),nums.end()).size();
     }
 };
