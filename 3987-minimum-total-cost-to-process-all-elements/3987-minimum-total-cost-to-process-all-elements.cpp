895b17aa160ebbc8e2ee44d1af17fbe6547e8215
Aditi Singh <singhaditi.as75@gmail.com>
2026-07-28 23:10:44 +0530

    Time: 0 ms (100%), Space: 169.3 MB (65.02%) - LeetHub

--- 3987-minimum-total-cost-to-process-all-elements/3987-minimum-total-cost-to-process-all-elements.cpp
+++ 3987-minimum-total-cost-to-process-all-elements/3987-minimum-total-cost-to-process-all-elements.cpp
@@ -3,7 +3,7 @@ public:
     int minimumCost(vector<int>& nums, int k) {
         long long MOD=1e9+7;
         long long currentres=k;
-        long long totalcost=0;
+        long long totaloper=0;
         for(int i=0;i<nums.size();i++){
             if(currentres>=nums[i]){
                 currentres=currentres-nums[i];
@@ -11,11 +11,11 @@ public:
             else {
                 long long requiredres=nums[i]-currentres;
                 long long requiredoper=(requiredres+k-1)/k;
-                totalcost=totalcost+requiredoper;
+                totaloper=totaloper+requiredoper;
                 currentres=currentres+(requiredoper*k)-nums[i];
             }
         }
-        return ((totalcost%MOD)*((totalcost+1)%MOD)/2)%MOD;
+        return ((totaloper%MOD)*((totaloper+1)%MOD)/2)%MOD;
         
     }
 };
