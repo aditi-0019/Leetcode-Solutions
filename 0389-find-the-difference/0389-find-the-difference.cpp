f63029247d26df89fe59c80ced72bb87bc509714
Aditi Singh <singhaditi.as75@gmail.com>
2026-08-22 00:22:51 +0530

    Time: 0 ms (100%), Space: 9.2 MB (51.87%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        int i=0,j=0;
-        while(i<s.size() && j<t.size()){
-            if(s[i]==t[j]){
-                i++; j++;
-            }
-            else 
-            return t[j]; //as t has extra char
-        }
-        // while(j<t.size()){
-        //     cout<<t[j];
-        //     j++;
-        // }
-        return t[j];
+        char ans = 0;
+        for(char c : s)
+            ans ^= c;
+        for(char c : t)
+            ans ^= c;
+        return ans;
     }
 };
