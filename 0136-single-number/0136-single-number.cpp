211acd362aa74db028876916aa0cea7f9d7f0b26
Aditi Singh <singhaditi.as75@gmail.com>
2026-08-29 17:41:06 +0530

    Time: 0 ms (100%), Space: 20.6 MB (58.74%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int,int>freq;
+        // using unordered map
+        // unordered_map<int,int>freq;
+        // for(int i=0;i<nums.size();i++){
+        //     freq[nums[i]]++;
+        // }
+        // for(int i=0;i<nums.size();i++){
+        // if(freq[nums[i]]==1)
+        // return nums[i];
+        // }
+        // return 0;
+
+        // XOR approach 
+        int XOR=0;
         for(int i=0;i<nums.size();i++){
-            freq[nums[i]]++;
+            XOR=XOR^nums[i];
         }
-        for(int i=0;i<nums.size();i++){
-        if(freq[nums[i]]==1)
-        return nums[i];
-        }
-        return 0;
+        return XOR;
     }
 };
