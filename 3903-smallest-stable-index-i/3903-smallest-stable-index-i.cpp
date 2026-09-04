8d79f36bdd93f7b72b7d64676823fd551b54a5bf
Aditi Singh <singhaditi.as75@gmail.com>
2026-09-04 23:20:22 +0530

    Time: 0 ms (100%), Space: 31.2 MB (46.94%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -1,32 +1,18 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        vector<int> score;
-        if (nums.size() == 1)
-            return 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int maxi = INT_MIN;
-            int mini = INT_MAX;
-            for (int j = 0; j <= i; j++) {
-                maxi = max(maxi, nums[j]);
-            }
-            for (int j = i; j < nums.size(); j++) {
-                mini = min(mini, nums[j]);
-            }
-            int diff = abs(maxi - mini);
-            score.push_back(diff);
+        int n=nums.size();
+        vector<int>minarr(n);
+        minarr[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            minarr[i]=min(nums[i],minarr[i+1]);
         }
-        // for(int i=0;i<score.size();i++){
-        //     int minimum=INT_MAX;
-        //     if(score[i]<=k){
-        //         minimum=min(minimum,score[i])
-        //     }
-        // }
-        for (int i = 0; i < score.size(); i++) {
-            if (score[i] <= k)
-                return i;
-        }
-    
-    return -1;
-}
+       int maxi=nums[0];
+       for(int i=0;i<n;i++){
+        maxi=max(maxi,nums[i]);
+        if(maxi-minarr[i]<=k)
+        return i;
+       } 
+       return -1;
+    }
 };
