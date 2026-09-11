267e9f0fb397392d4daf615cf46c4c7b889d7ece
Aditi Singh <singhaditi.as75@gmail.com>
2026-09-11 11:31:18 +0530

    Time: 0 ms (100%), Space: 8.1 MB (54.82%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int left=0;
+        int right=s.size()-1;
+        while(left<right){
+            if(left<right && !isalpha(s[left])){
+            left++;
+            continue;
+            }
+            if(left<right && !isalpha(s[right])){
+                right--;
+                continue;
+            }
+        
+            swap(s[left],s[right]);
+            left++;
+            right--;
+        
+        }
+        return s;
+    }
+};
